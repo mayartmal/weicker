608103372e3408e399927d7ce5548216d4f2f5cc
mayartmal <mayartmal@gmail.com>
2025-01-13 18:12:19 +0400

    Initial commit

---
+++ resources/weicker_tools.py
@@ -0,0 +1,37 @@
+#function to add weight to journal
+
+#function to read journal
+
+#function to get one weight record
+
+#function to edit one weight record
+
+
+#class journal
+#initialize from json
+#add weight
+#save record
+#get record
+#edit record
+#read record
+#read set of records
+
+
+class Weicker:
+    def __init__(self, storage_link):
+        self.storage_link = storage_link
+
+    def get_all_records(self):
+        pass
+
+    def get_record(self):
+        pass
+
+    def get_window_of_records(self):
+        pass
+
+    def add_record(self):
+        pass
+
+    def edit_record(self):
+        pass
